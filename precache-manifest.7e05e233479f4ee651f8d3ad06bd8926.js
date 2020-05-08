6f16326c1c387ff0f152ced12c76fb29e60f400d
Igor Zientalski <33549555+iGorrrrrrr@users.noreply.github.com>
2020-05-08 15:04:34 +0200

    Updates

---
+++ precache-manifest.7e05e233479f4ee651f8d3ad06bd8926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd4fa9b6712f983654786f849c3dd14",
+    "url": "/veloloco/index.html"
+  },
+  {
+    "revision": "fd2bb03c3ae27d406351",
+    "url": "/veloloco/static/css/main.50ba8d7a.chunk.css"
+  },
+  {
+    "revision": "43305adfc1d0ac75f0c0",
+    "url": "/veloloco/static/js/2.d935757d.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/veloloco/static/js/2.d935757d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2bb03c3ae27d406351",
+    "url": "/veloloco/static/js/main.cf96f5f8.chunk.js"
+  },
+  {
+    "revision": "3e7bbaa1180d13309f4a",
+    "url": "/veloloco/static/js/runtime-main.4b6efefd.js"
+  }
+]);
